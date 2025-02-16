6036b59ffe58d0f9e371d91ea2ad56fab262881d
vinayak <vinayak.kurugovina@gmail.com>
2025-02-17 00:18:24 +0530

    'Updates'

--- src/errors/not-found-error.ts
+++ src/errors/not-found-error.ts
@@ -1,7 +1,7 @@
 import { CustomError } from "./custom-error";
 
 export class NotFoundError extends CustomError {
-  statusCode = 500;
+  statusCode = 404;
   reason = 'Not Found'
   constructor() {
     super('Route not found')
